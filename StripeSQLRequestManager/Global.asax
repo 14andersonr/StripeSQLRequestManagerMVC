47e30fad3c370389b7f978e4c1ec0248fa8a19b1
Richard Anderson III <14andersonr@gmail.com>
2021-04-22 18:27:23 -0400

    Add project files.

---
+++ StripeSQLRequestManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StripeSQLRequestManager.MvcApplication" Language="C#" %>
---
+++ StripeSQLRequestManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StripeSQLRequestManager.Startup))]
+namespace StripeSQLRequestManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
